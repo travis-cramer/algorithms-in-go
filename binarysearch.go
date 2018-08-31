27bf4a8c767fccdff1d04ac080df8501e3bc5e17
travis-cramer <travis@ridecell.com>
2018-08-31 08:28:00 -0700

    add test_binarysearch function

--- binarysearch.go
+++ binarysearch.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+
+func main() {
+	test_binarysearch()
+}
+
+
 func binarysearch(array []string, target string) (index int) {
 	var L int
 	L = 0
@@ -25,10 +31,19 @@ func binarysearch(array []string, target string) (index int) {
 	return
 }
 
-func main() {
+
+func test_binarysearch() {
 	// tests binarysearch, output should be 0...(len(array) - 1)
-	array := [10]string{"apple", "banana", "carrot", "danish", "energy", "fun", "greatness", "hairy", "igneous", "jack"}
+	array := [10]string{"apple", "banana", "carrot", "danish", "energy", "fun", "greatness", "hairy", "igneous", "jim"}
 	for i := 0; i < len(array); i++ {
-		fmt.Println(binarysearch(array[:], array[i]))
+		var index int
+		index = binarysearch(array[:], array[i])
+		if index != i {
+			fmt.Println("Failed.")
+			return
+		}
 	}
+	fmt.Println("Success!")
+	return
 }
+
